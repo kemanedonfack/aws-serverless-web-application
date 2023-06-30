b758142a20850ef698ad6c7641f05800b92ad912
Kemane Donfack <kemanedonfack5@gmail.com>
2023-06-30 21:20:12 +0100

    the website works

--- scripts.js
+++ scripts.js
@@ -1,45 +1,55 @@
 //add your api here below
-var API_ENDPOINT = "https://flsee7mskl.execute-api.eu-north-1.amazonaws.com/Production"
+var API_ENDPOINT = "https://yl7ealf6a8.execute-api.eu-north-1.amazonaws.com/v1"
 //AJAX GET REQUEST
-document.getElementById("saveprofile").onclick = function(){
+document.getElementById("createEmployeeForm").onsubmit = function(event) {
+  event.preventDefault(); // Pour empêcher le formulaire de se soumettre normalement
   var inputData = {
-    "empId":$('#empid').val(),
-        "empFirstName":$('#fname').val(),
-        "empLastName":$('#lname').val(),
-    "empAge":$('#empage').val()           
-      };
+    "employeeId": $('#id').val(),
+    "name": $('#name').val(),
+    "email": $('#email').val(),
+    "address": $('#address').val(),
+    "phone": $('#phone').val()
+  };
+
   $.ajax({
-        url: API_ENDPOINT,
-        type: 'POST',
-        data:  JSON.stringify(inputData)  ,
-        contentType: 'application/json; charset=utf-8',
-        success: function (response) {
-          document.getElementById("profileSaved").innerHTML = "Profile Saved!";
-        },
-        error: function () {
-            alert("error");
-        }
-    });
-}
+    url: API_ENDPOINT,
+    type: 'POST',
+    data: JSON.stringify(inputData),
+    contentType: 'application/json; charset=utf-8',
+    success: function(response) {
+      $('#createEmployeeForm')[0].reset();
+      alert("success");
+    },
+    error: function() {
+      alert("error");
+    }
+  });
+};
+
 //AJAX GET REQUEST
-document.getElementById("getprofile").onclick = function(){  
+document.getElementById("getEmployees").onclick = function() {
   $.ajax({
-        url: API_ENDPOINT,
-        type: 'GET',
-         contentType: 'application/json; charset=utf-8',
-        success: function (response) {
-          $('#employeeProfile tr').slice(1).remove();
-          jQuery.each(response, function(i,data) {          
-            $("#employeeProfile").append("<tr> \
-                <td>" + data['empId'] + "</td> \
-                <td>" + data['empFirstName'] + "</td> \
-                <td>" + data['empLastName'] + "</td> \
-                <td>" + data['empAge'] + "</td> \
-                </tr>");
-          });
-        },
-        error: function () {
-            alert("error");
-        }
-    });
-}
+    url: API_ENDPOINT,
+    type: 'GET',
+    contentType: 'application/json; charset=utf-8',
+    success: function(response) {
+      $('#EmployeesTable tr').slice(1).remove();
+      jQuery.each(response, function(i, data) {
+        var row = "<tr> \
+          <td>" + data['employeeId'] + "</td>\
+          <td>" + data['name'] + "</td>\
+          <td>" + data['email'] + "</td>\
+          <td>" + data['address'] + "</td>\
+          <td>" + data['phone'] + "</td>\
+          <td>\
+            <a href='#deleteEmployeeModal' class='delete' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='Delete'>&#xE872;</i></a>\
+          </td>\
+        </tr>";
+        $("#EmployeesTable").append(row);
+      });
+    },
+    error: function() {
+      alert("error");
+    }
+  });
+};
